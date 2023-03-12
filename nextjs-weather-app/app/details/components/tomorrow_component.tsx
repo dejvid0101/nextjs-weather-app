77feee74f59224bb2ffe7aedfb79747784620c5d
David Koren <dejvid0101@gmail.com>
2023-03-12 21:49:25 +0100

    current weather component finished

--- nextjs-weather-app/app/details/components/tomorrow_component.tsx
+++ nextjs-weather-app/app/details/components/tomorrow_component.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function tomorrow() {
   return (
-    <div className='float-left inline w-11/12 antialiased h-96 bg-white border-2 border-indigo-400 rounded-md'>
+    <div className='float-left inline w-11/12 h-96 bg-white border-2 border-indigo-400 rounded-md'>
     
     </div>
   )
--- nextjs-weather-app/app/details/page.tsx
+++ nextjs-weather-app/app/details/page.tsx
@@ -1,41 +1,48 @@
 import React from 'react'
 import Currentweather from './components/currentweather'
 import Forecast3days from './components/forecast3days'
-import HourlyComponent from './components/hourly_component';
+import TodayComponent from './components/today_component';
 import TomorrowComponent from './components/tomorrow_component';
 
-async function getCurrentWeatherInfo(param:any){
-//fetch object with coordinates based on city name in request params
-const responseCoordinates=await fetch('https://api.opencagedata.com/geocode/v1/json?q='+param+'&key=dfbba9f7d48b4dbfb72e5ff1ae21d4ad',
+//ADD ERROR HANDLING FOR WHEN URL PARAMETER IS NOT SUPPLIED
+
+async function fetchCoordinates(cityName:any) {
+  const responseCoordinates=await fetch('https://api.opencagedata.com/geocode/v1/json?q='+cityName+'&key=dfbba9f7d48b4dbfb72e5ff1ae21d4ad',
 {cache:'no-store'});
 const dataCoordinates=await responseCoordinates.json();
+return dataCoordinates;
+}
 
-const fetchedCityObj=dataCoordinates;
+async function getWeatherInfo(cityName:any){
+//fetch object with coordinates based on city name in request params
+const fetchedCityObj=await fetchCoordinates(cityName);
 const lat=fetchedCityObj.results[0].geometry.lat;
 const lng=fetchedCityObj.results[0].geometry.lng;
 
 //fetch current weather object based on coordinates
-const responseWeather=await fetch('https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+lng+'&current_weather=true',
+const responseWeather=await fetch('https://api.open-meteo.com/v1/forecast?latitude='+lat+'&longitude='+lng+'&current_weather=true&daily=weathercode&daily=temperature_2m_min&daily=temperature_2m_max&timezone=auto&daily=apparent_temperature_max&daily=apparent_temperature_min&daily=precipitation_probability_max',
 {cache:'no-store'});
 const dataWeather=await responseWeather.json();
 return dataWeather;
 }
 
+
 //page server component receives url params through props
 export default async function page({searchParams}: {searchParams?: { [key: string]: string | string[] | undefined };}) {
 
   // add error handling for when url paramaters aren't supplied or incorrect
-const currentWeatherObject=await getCurrentWeatherInfo(searchParams?.name);
-console.log(currentWeatherObject);
+const weatherObject=await getWeatherInfo(searchParams?.name);
 
 //weather details for location
     return (
-    <div className="container grid mx-4 lg:mx-32 w-auto h-auto bg-violet-200 gap-y-4 lg:gap-4 lg:grid-cols-2">
+    <div className="container grid mx-4 lg:mx-32 w-auto h-auto bg-violet-100 gap-y-4 lg:gap-4 lg:grid-cols-2">
 <div className="text-indigo-800 h-32 text-4xl antialiased text-5xl font-poppins grid items-center lg:col-span-2">{searchParams?.name}, {searchParams?.region}</div>
-<Currentweather WeatherProp={currentWeatherObject}></Currentweather>
-<HourlyComponent></HourlyComponent>
+<Currentweather WeatherProp={weatherObject}></Currentweather>
+<TodayComponent WeatherProp={weatherObject}></TodayComponent>
 <TomorrowComponent></TomorrowComponent>
 <Forecast3days></Forecast3days>
     </div>
   )
 }
+
+
